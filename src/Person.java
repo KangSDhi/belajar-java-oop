42061f4b47defde256676c4ae2286c6073d26fa3
KangSDhi <kangteknisi@gmail.com>
2022-12-03 19:53:32 +0700

    Constructor

--- src/Person.java
+++ src/Person.java
@@ -3,6 +3,11 @@ class Person {
     String address;
     final String country = "Indonesia";
 
+    Person(String paramName, String paramAddress){
+        name = paramName;
+        address = paramAddress;
+    }
+
     void sayHello(String paramName){
         System.out.println("Hello " + paramName + ", My Name is " + name);
     }
--- src/PersonApp.java
+++ src/PersonApp.java
@@ -1,7 +1,7 @@
 public class PersonApp {
     public static void main(String[] args) {
 
-        var person1 = new Person();
+        var person1 = new Person("Sig", "tar");
         person1.name = "Sigit";
         person1.address = "Tulungrejo";
 //        person1.country = "Amerika"; // ERROR
@@ -12,9 +12,11 @@ public class PersonApp {
 
         person1.sayHello("Bodi");
 
-        Person person2 = new Person();
+        Person person2 = new Person("Sig", "Tar");
+        person2.sayHello("Tiwuk");
+
         Person person3;
-        person3 = new Person();
+        person3 = new Person("Sig", "Nar");
 
         person3.name = "Tiwuk";
         person3.sayHello("Turi");
