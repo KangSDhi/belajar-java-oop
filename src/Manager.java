cda08a3c55e611730c955913db2d07cc63cfcf04
KangSDhi <kangteknisi@gmail.com>
2022-12-07 13:44:15 +0700

    Method Overriding

--- src/Manager.java
+++ src/Manager.java
@@ -3,7 +3,7 @@ class Manager {
     String name;
 
     void sayHello(String name){
-        System.out.println("Hi " + name + ", My Name Is " + this.name);
+        System.out.println("Hi " + name + ", My Name Is Manager " + this.name);
     }
 
 }
--- src/VicePresident.java
+++ src/VicePresident.java
@@ -1,2 +1,7 @@
 class VicePresident extends Manager{
+
+    void sayHello(String name){
+        System.out.println("Hello " + name + ", My Name is VP " + this.name);
+    }
+
 }
