d1e653bf9fff1809d92aa61f93a8ba70096b2e85
KangSDhi <kangteknisi@gmail.com>
2022-12-08 09:19:32 +0700

    Type Check dan Casts

--- src/PolymorphismApp.java
+++ src/PolymorphismApp.java
@@ -12,10 +12,18 @@ public class PolymorphismApp {
 
         sayHello(new Employee("Eko"));
         sayHello(new Manager("Eko"));
-        sayHello(new Manager("Budi"));
+        sayHello(new VicePresident("Budi"));
     }
 
-    static void sayHello(Employee employee){
-        System.out.println("Hello " + employee.name);
+    static void sayHello(Employee employee) {
+        if (employee instanceof VicePresident) {
+            VicePresident vicePresident = (VicePresident) employee;
+            System.out.println("Hello VP " + vicePresident.name);
+        } else if (employee instanceof Manager) {
+            Manager manager = (Manager) employee;
+            System.out.println("Hello Manager " + manager.name);
+        } else {
+            System.out.println("Hello " + employee.name);
+        }
     }
 }
