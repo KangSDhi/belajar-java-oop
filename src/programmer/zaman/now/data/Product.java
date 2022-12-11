1cc5a55252b7c3b91781d89a69087e245a284eaa
KangSDhi <kangteknisi@gmail.com>
2022-12-11 09:18:59 +0700

    Access Modifier

--- src/programmer/zaman/now/data/Product.java
+++ src/programmer/zaman/now/data/Product.java
@@ -2,10 +2,12 @@ package programmer.zaman.now.data;
 
 public class Product {
 
-    String name;
-    int price;
+//    protected String name;
+    public String name;
+//    protected int price;
+    public int price;
 
-    Product(String name, int price){
+    public Product(String name, int price){
         this.name = name;
         this.price = price;
     }
