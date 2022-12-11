c5f1df79a0556be366cf16c792e39ccf8ae817c2
KangSDhi <kangteknisi@gmail.com>
2022-12-11 19:51:53 +0700

    Interface Inheritance

--- src/programmer/zaman/now/data/Avanza.java
+++ src/programmer/zaman/now/data/Avanza.java
@@ -8,4 +8,12 @@ public class Avanza implements Car{
     public int getTire() {
         return 4;
     }
+
+    public String getBrand() {
+        return "Toyota";
+    }
+
+    public boolean isMaintenance() {
+        return false;
+    }
 }
--- src/programmer/zaman/now/data/Car.java
+++ src/programmer/zaman/now/data/Car.java
@@ -1,6 +1,6 @@
 package programmer.zaman.now.data;
 
-public interface Car {
+public interface Car extends HasBrand, IsMaintenance{
     void drive();
 
     int getTire();
