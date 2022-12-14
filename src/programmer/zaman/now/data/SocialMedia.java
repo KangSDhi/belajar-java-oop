7a1586429eb01874fbeedef5e0889e38c876742e
KangSDhi <kangteknisi@gmail.com>
2022-12-14 08:45:14 +0700

    Final Class

---
+++ src/programmer/zaman/now/data/SocialMedia.java
@@ -0,0 +1,14 @@
+package programmer.zaman.now.data;
+
+class SocialMedia {
+    String name;
+}
+
+final class Facebook extends SocialMedia{
+
+}
+
+// ERROR
+//class FakeFacebook extends Facebook {
+//
+//}
