27132f0bd43487bcea8907607e3c3cede635e146
KangSDhi <kangteknisi@gmail.com>
2022-12-19 11:48:38 +0700

    Record Class

---
+++ src/programmer/zaman/now/data/LoginRequest.java
@@ -0,0 +1,16 @@
+package programmer.zaman.now.data;
+
+public record LoginRequest(String username, String password) {
+
+    public LoginRequest {
+        System.out.println("Membuat object LoginRequest");
+    }
+
+    public LoginRequest(String username){
+        this(username, "");
+    }
+
+    public LoginRequest(){
+        this("", "");
+    }
+}
