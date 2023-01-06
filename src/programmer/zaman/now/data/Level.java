cf0c5187a958a85b73509114854e9718a66e6f4d
KangSDhi <kangteknisi@gmail.com>
2023-01-06 08:41:47 +0700

    Annotation

--- src/programmer/zaman/now/data/Level.java
+++ src/programmer/zaman/now/data/Level.java
@@ -1,5 +1,8 @@
 package programmer.zaman.now.data;
 
+import programmer.zaman.now.annotation.Fancy;
+
+@Fancy(name = "Level", tags = {"application", "java"})
 public enum Level {
     STANDARD("Standard Level"),
     PREMIUM("Premium Level"),
--- src/programmer/zaman/now/error/BlankException.java
+++ src/programmer/zaman/now/error/BlankException.java
@@ -4,4 +4,9 @@ public class BlankException extends RuntimeException{
     public BlankException(String message){
         super(message);
     }
+
+    @Override
+    public String getMessage() {
+        return super.getMessage();
+    }
 }
