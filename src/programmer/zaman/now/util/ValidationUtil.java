ce6b200539bd0aaeb0aeedd4a4f6047a86af6df4
KangSDhi <kangteknisi@gmail.com>
2023-01-06 10:07:24 +0700

    Reflection

--- src/programmer/zaman/now/util/ValidationUtil.java
+++ src/programmer/zaman/now/util/ValidationUtil.java
@@ -1,8 +1,11 @@
 package programmer.zaman.now.util;
 
+import programmer.zaman.now.annotation.NotBlank;
 import programmer.zaman.now.data.LoginRequest;
 import programmer.zaman.now.error.BlankException;
 import programmer.zaman.now.error.ValidationException;
+
+import java.lang.reflect.Field;
 
 public class ValidationUtil {
     public static void validate(LoginRequest loginRequest) throws ValidationException, NullPointerException {
@@ -26,6 +29,27 @@ public class ValidationUtil {
             throw new NullPointerException("Password is null");
         }else if (loginRequest.password().isBlank()){
             throw new BlankException("Password is blank");
+        }
+    }
+
+    public static void validationReflection(Object object){
+        Class aClass = object.getClass();
+        Field[] fields = aClass.getDeclaredFields();
+
+        for (var field : fields){
+            field.setAccessible(true);
+
+            if (field.getAnnotation(NotBlank.class) != null){
+                // validated
+                try {
+                    String value = (String) field.get(object);
+                    if (value == null || value.isBlank()){
+                        throw new BlankException("Field " + field.getName() + " is blank");
+                    }
+                }catch (IllegalAccessException exception){
+                    System.out.println("Tidak bisa mengkases field " + field.getName());
+                }
+            }
         }
     }
 }
